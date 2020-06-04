af03293887319180ce11bbfd13a765d53da30f5f
Elizara <elizara.georgieva@gmail.com>
2020-06-04 12:18:34 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require
+//var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
     return gulp.src('./scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./css'))
+    // .pipe(browserSync.reload({
+    //     stream: true
+    // }))
 }); 
 
 gulp.task('watch', function(){
     gulp.watch('./scss/**/*.scss', gulp.parallel('sass'));
 })
 
-
+// gulp.task('browserSync', function(){
+//     browserSync.init({
+//         server: {
+//             baseDir: 'project'
+//         },
+//     })
+// })
 
